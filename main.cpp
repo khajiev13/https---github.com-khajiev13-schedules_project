230038f81b5f4af92e88b4effc6b9d6070810265
Roma <57835288+khajiev13@users.noreply.github.com>
2022-06-23 12:11:09 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 
 int main()
 {
-    std::cout << "Hello CPP!" << std::endl;
+    std::cout << "Hello CPP!" << std::endl; 
+    std::cout << "These are the changes from github!!!" << std::endl;
     return 1;
 }
