eb7f23c2ac25fedbd7f557edd0cf6eac3c0f961c
Roma <57835288+khajiev13@users.noreply.github.com>
2022-06-26 12:00:08 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,5 +4,6 @@ int main()
 {
     std::cout << "Hello CPP!" << std::endl;
     std::cout << "This is another line of code!" << std::endl;
+    std::cout << "Change from github website!" << std::endl;
     return 1;
 }
